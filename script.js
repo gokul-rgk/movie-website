44de5eb3087da3c47359799b0d89040f03d333e4
gokul-rgk <108220961+gokul-rgk@users.noreply.github.com>
2023-05-08 14:01:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-//Initial References
+
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
-//Function to fetch data from API
 let getMovie = () => {
   let movieName = movieNameRef.value;
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-  //If input field is empty
+
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
   }
-  //If input field is NOT empty
+  
   else {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        //If movie exists in database
+        
         if (data.Response == "True") {
           result.innerHTML = `
             <div class="info">
@@ -44,12 +43,12 @@ let getMovie = () => {
             
         `;
         }
-        //If movie does NOT exists in database
+      
         else {
           result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
         }
       })
-      //If error occurs
+  
       .catch(() => {
         result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
       });
